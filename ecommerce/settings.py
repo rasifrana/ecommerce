ef7866ce7be59d3b091860895a56ffe188430d8a
Asif Rana <rasifrana@hotmail.com>
2020-03-27 01:04:11 +0000

    added new apps cart and products

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_forms_bootstrap',
-    'accounts'
+    'accounts',
+    'products',
+    'cart'
 ]
 
 MIDDLEWARE = [
@@ -64,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -125,6 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
